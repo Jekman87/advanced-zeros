5bec7079aabd7b941f0929375a225200811fa308
Jekman <jeka_zzz@mail.ru>
2019-03-16 07:07:09 +0300

    style: now good)

--- src/index.js
+++ src/index.js
@@ -3,51 +3,43 @@ module.exports = function getZerosCount(number, base) {
   let i = 1;
   let obj = {};
 
-  //факторизуем базу и записываем данные в объект, 
-  //где i - фактор и значение ключа, j - степень и название ключа
+  //Факторизуем базу и записываем данные в объект, где свойство - фактор (i), а ключ - степень (j)
+  //таким образом для одинаковых показателей степени (ключ) мы оставляем
+  //только максимальные основания (свойство), т.к. они перезаписываются последними
   do {
+    let j = 0;
     i++;
     if (remainder % i) continue;
 
-    let j = 0;
     do {
       remainder /= i
       j++;
     } while (!(remainder % i));
 
     obj[j] = i;
-
   } while ((i * i < base) && (remainder !== 1));
 
   if (remainder > 1) obj[1] = remainder;
 
+  let zeroArr = [];
 
-  
-  let arr = [];
-
-
+  //По формуле Лежандра находим количество нулей
   for (let key in obj) {
     i = 1;
     let sum = 0;
-    let element = 0;
+    let step = 0;
 
     while (true) {
-      element = Math.floor(number / Math.pow(obj[key], i));
+      step = Math.floor(number / Math.pow(obj[key], i));
 
-      if (element === 0) break;
+      if (step === 0) break;
 
-      sum += element;
+      sum += step;
       i++;
     }
 
-    arr.push( Math.floor(sum / +key) );
-
-
-
+    zeroArr.push( Math.floor(sum / +key) );
   }
 
-   
-   
-
-  return Math.min(...arr);
+  return Math.min(...zeroArr);
 }
