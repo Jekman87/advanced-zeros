9a5fb60aab4480ab449528a582840b35eaa6b8f0
Jekman <jeka_zzz@mail.ru>
2019-03-16 06:46:24 +0300

    feat: Implemented getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let remainder = base;
+  let i = 1;
+  let obj = {};
+
+  //факторизуем базу и записываем данные в объект, 
+  //где i - фактор и значение ключа, j - степень и название ключа
+  do {
+    i++;
+    if (remainder % i) continue;
+
+    let j = 0;
+    do {
+      remainder /= i
+      j++;
+    } while (!(remainder % i));
+
+    obj[j] = i;
+
+  } while ((i * i < base) && (remainder !== 1));
+
+  if (remainder > 1) obj[1] = remainder;
+
+
+  
+  let arr = [];
+
+
+  for (let key in obj) {
+    i = 1;
+    let sum = 0;
+    let element = 0;
+
+    while (true) {
+      element = Math.floor(number / Math.pow(obj[key], i));
+
+      if (element === 0) break;
+
+      sum += element;
+      i++;
+    }
+
+    arr.push( Math.floor(sum / +key) );
+
+
+
+  }
+
+   
+   
+
+  return Math.min(...arr);
 }
